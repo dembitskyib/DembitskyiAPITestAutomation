5a0d599c1de4e60cb063b758c813f1a741e52dd5
Bohdan Dembitskyi <Bohdan_Dembitskyi@epam.com>
2019-03-25 13:53:08 +0200

    Changed constant interface to constant class

--- src/main/java/com/goodreads/allure/AllureEnvironmentController.java
+++ src/main/java/com/goodreads/allure/AllureEnvironmentController.java
@@ -1,6 +1,6 @@
 package com.goodreads.allure;
 
-import com.goodreads.environment.Environment;
+import com.goodreads.environment.EnvironmentConstants;
 import com.goodreads.environment.PropertiesController;
 
 import java.util.Properties;
@@ -8,8 +8,8 @@ import java.util.Properties;
 public class AllureEnvironmentController {
 
     public void createPropertyFile() {
-        Properties environmentProperties = PropertiesController.loadProperties(Environment.INPUT_ENVIRONMENT_PROPERTIES_PATH);
-        PropertiesController.createPropertyFile(Environment.OUTPUT_ENVIRONMENT_PROPERTIES_PATH, environmentProperties);
+        Properties environmentProperties = PropertiesController.loadProperties(EnvironmentConstants.INPUT_ENVIRONMENT_PROPERTIES_PATH);
+        PropertiesController.createPropertyFile(EnvironmentConstants.OUTPUT_ENVIRONMENT_PROPERTIES_PATH, environmentProperties);
     }
 
 }
--- src/main/java/com/goodreads/environment/EnvironmentConstants.java
+++ src/main/java/com/goodreads/environment/EnvironmentConstants.java
@@ -2,13 +2,16 @@ package com.goodreads.environment;
 
 import java.util.Properties;
 
-public interface Environment {
+public class EnvironmentConstants {
 
-    Properties MAIN_RESOURCES_PROPERTIES = PropertiesController
+    private EnvironmentConstants() {
+    }
+
+    private static final Properties MAIN_RESOURCES_PROPERTIES = PropertiesController
             .loadProperties("src/main/resources/resources.properties");
-    String INPUT_ENVIRONMENT_PROPERTIES_PATH
+    public static final String INPUT_ENVIRONMENT_PROPERTIES_PATH
             = MAIN_RESOURCES_PROPERTIES.getProperty("INPUT_ENVIRONMENT_PROPERTIES_PATH");
-    String OUTPUT_ENVIRONMENT_PROPERTIES_PATH
+    public static final String OUTPUT_ENVIRONMENT_PROPERTIES_PATH
             = MAIN_RESOURCES_PROPERTIES.getProperty("OUTPUT_ENVIRONMENT_PROPERTIES_PATH");
 
 }
